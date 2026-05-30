0027d1f899851b80ce65444efc4a1a091400cd69
league <bbbbb@bbbbb>
2026-05-30 14:27:57 -0700

    bhbhbhbhbhbhb

--- _01_lists/_b_intro_to_lists.py
+++ _01_lists/_b_intro_to_lists.py
@@ -6,8 +6,7 @@ import unittest
 
 # TODO Complete the function so it returns a list of any 5 items
 def test_1_create_a_list():
-    return None
-
+    return list[range(0, 21)]
 
 # TODO Complete the function so it returns a list of numbers from 0 to 20
 #  with both numbers 0 and 20 included in the list
